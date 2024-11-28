3caf426d6144ff04f7d6f12f2d02284079b53be8
Dan Anglin <daangling@gmail.com>
2024-11-28 21:33:07 +0000

    test: increase test coverage in discovery package
    - Created mock websites to test discovering client metadata from HTML
      and JSON.
    - Added a test for clients responding with a bad status code.
    
    PR: https://codeflow.dananglin.me.uk/apollo/beacon/pulls/29

--- internal/discovery/discovery_test.go
+++ internal/discovery/discovery_test.go
@@ -5,8 +5,10 @@
 package discovery_test
 
 import (
-	"net/url"
-	"os"
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 
@@ -14,55 +16,170 @@ import (
 )
 
 func TestGetMetadataFromHTML(t *testing.T) {
-	filename := "testdata/TestWebsite.golden"
+	t.Parallel()
 
-	file, err := os.Open(filename)
+	t.Run("Discovery via HTML", testGetMetadataFromHTML)
+	t.Run("Discovery via JSON", testGetMetadataFromJSON)
+	t.Run("Bad status code from client", testBadStatusCode)
+}
+
+func testGetMetadataFromHTML(t *testing.T) {
+	t.Parallel()
+
+	testHTMLWebsite := `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Test Webpage</title>
+        <link rel="redirect_uri" href="/callback">
+        <link rel="redirect_uri" href="http://callback.testclient.example">
+    </head>
+
+    <body>
+      <header>
+        <div class="h-app">
+          <img src="assets/logo.png" class="u-logo">
+          <a href="/" class="u-url p-name">Test Client Website</a>
+        </div>
+      </header>
+      <h1>Test Webpage</h1>
+      <p>A simple paragraph</p>
+    </body>
+</html>
+`
+
+	testClient := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, _ *http.Request) {
+		writer.Header().Set("Content-Type", "text/html; charset=UTF-8")
+		writer.WriteHeader(http.StatusOK)
+		_, _ = writer.Write([]byte(testHTMLWebsite))
+	}))
+	defer testClient.Close()
+
+	gotMetadata, err := discovery.FetchClientMetadata(context.Background(), testClient.URL)
 	if err != nil {
 		t.Fatalf(
-			"FAILED test %s: unable to open %s: %v",
+			"FAILED test %s: Received an error after attempting to get the client's metadata.\ngot: %q",
 			t.Name(),
-			filename,
-			err,
+			err.Error(),
 		)
 	}
-	defer file.Close()
-
-	clientID := "https://test.website.net/"
-
-	parsedClientID, err := url.Parse(clientID)
-	if err != nil {
-		t.Fatalf(
-			"FAILED test %s: unable to parse %s: %v",
-			t.Name(),
-			clientID,
-			err,
-		)
-	}
-
-	gotMetadata := discovery.GetMetadataFromHTML(file, clientID, parsedClientID)
 
 	wantMetadata := discovery.ClientIDMetadata{
-		ClientID:   "https://test.website.net/",
-		ClientName: "Test Website",
-		ClientURI:  "https://test.website.net/",
-		LogoURI:    "https://test.website.net/assets/logo.png",
+		ClientID:   testClient.URL,
+		ClientName: "Test Client Website",
+		ClientURI:  testClient.URL + "/",
+		LogoURI:    testClient.URL + "/assets/logo.png",
 		RedirectURIs: []string{
-			"https://test.website.net/redirect",
-			"https://redirect.website.net/",
+			testClient.URL + "/callback",
+			"http://callback.testclient.example",
 		},
 	}
 
 	if !reflect.DeepEqual(wantMetadata, gotMetadata) {
 		t.Errorf(
-			"FAILED test %s: unexpected metadata received:\nwant: %+v\ngot:  %+v",
+			"FAILED test %s: unexpected metadata received.\nwant: %+v\ngot: %+v",
 			t.Name(),
 			wantMetadata,
 			gotMetadata,
 		)
 	} else {
 		t.Logf(
-			"Expected metadata received:\ngot:  %+v",
+			"Expected metadata received.\ngot: %+v",
 			gotMetadata,
+		)
+	}
+}
+
+func testGetMetadataFromJSON(t *testing.T) {
+	t.Parallel()
+
+	testClientMetadataString := `
+{
+	"client_id": "https://testclient.example/",
+	"client_name": "Test Client Website",
+	"client_uri": "https://testclient.example/",
+	"logo_uri": "https://testclient.example/static/logo.png",
+	"redirect_uris": [
+		"https://testclient.example/redirect",
+		"https://testclient.example/callback",
+		"https://callback.testclient.example/"
+	]
+}
+`
+
+	testClient := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, _ *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		_, _ = writer.Write([]byte(testClientMetadataString))
+	}))
+	defer testClient.Close()
+
+	gotMetadata, err := discovery.FetchClientMetadata(context.Background(), testClient.URL)
+	if err != nil {
+		t.Fatalf(
+			"FAILED test %s: Received an error after attempting to get the client's metadata.\ngot: %q",
+			t.Name(),
+			err.Error(),
+		)
+	}
+
+	wantMetadata := discovery.ClientIDMetadata{
+		ClientID:   "https://testclient.example/",
+		ClientName: "Test Client Website",
+		ClientURI:  "https://testclient.example/",
+		LogoURI:    "https://testclient.example/static/logo.png",
+		RedirectURIs: []string{
+			"https://testclient.example/redirect",
+			"https://testclient.example/callback",
+			"https://callback.testclient.example/",
+		},
+	}
+
+	if !reflect.DeepEqual(wantMetadata, gotMetadata) {
+		t.Errorf(
+			"FAILED test %s: unexpected metadata received.\nwant: %+v\ngot: %+v",
+			t.Name(),
+			wantMetadata,
+			gotMetadata,
+		)
+	} else {
+		t.Logf(
+			"Expected metadata received.\ngot: %+v",
+			gotMetadata,
+		)
+	}
+}
+
+func testBadStatusCode(t *testing.T) {
+	t.Parallel()
+
+	testClient := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, _ *http.Request) {
+		writer.Header().Set("Content-Type", "text/html")
+		writer.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer testClient.Close()
+
+	_, err := discovery.FetchClientMetadata(context.Background(), testClient.URL)
+	if err == nil {
+		t.Fatalf(
+			"FAILED test %s: Did not receive an error for bad status code.",
+			t.Name(),
+		)
+	}
+
+	wantErr := discovery.BadStatusResponseError{}
+
+	if !errors.As(err, &wantErr) {
+		t.Errorf(
+			"FAILED test %s: Unexpected error received for bad status code.\nwant something like: %q\ngot: %q",
+			t.Name(),
+			wantErr.Error(),
+			err.Error(),
+		)
+	} else {
+		t.Logf(
+			"Expected error received for bad status code.\ngot: %q",
+			err.Error(),
 		)
 	}
 }
