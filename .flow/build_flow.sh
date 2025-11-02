b30277cef30ae50a2d29edb8f6a871a489d00623
Dan Anglin <daangling@gmail.com>
2025-11-02 19:10:52 +0000

    chore: add .flow/build_flow.sh
    Add the build script for the deployment onto the Flow Platform.

---
+++ .flow/build_flow.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This is a build script used as part of the deployment
+# of Beacon to the Flow Platform.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+mage="go tool -modfile=./tools/tools.mod mage"
+
+mkdir -p "${FLOW_SERVICE_BUILD_DIR}"
+${mage} clean build
+mv "__build/${BEACON_APP_NAME}" "${FLOW_SERVICE_BUILD_DIR}/${BEACON_APP_NAME}"
+echo "Successfully moved the binary to ${FLOW_SERVICE_BUILD_DIR}/${BEACON_APP_NAME}."
